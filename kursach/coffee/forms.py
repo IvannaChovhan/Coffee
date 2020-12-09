b5c4e3b59a005cc733aba9b9f348d519aa69cf2e
IvannaChovhan <ivanna.chovhan131@gmail.com>
2020-12-09 16:10:47 +0200

    Installed theme with table and charts pages

--- kursach/coffee/forms.py
+++ kursach/coffee/forms.py
@@ -1,12 +1,60 @@
 import django.forms as forms
 import datetime
+from .models import *
 
 from kursach.coffee.models import current_year
 
 
-def year_choices():
-    return [(r, r) for r in range(1984, datetime.date.today().year + 1)]
+class CountryForm(ModelForm):
+    class Meta:
+        model = Country
+        fields = ['nameCountry']
 
 
-class MyForm(forms.ModelForm):
-    year = forms.TypedChoiceField(coerce=int, choices=year_choices, initial=current_year)
+class CoffeeTypeForm(ModelForm):
+    class Meta:
+        model = CoffeeType
+        fields = ['nameCoffeeType']
+
+
+class BuyerForm(ModelForm):
+    class Meta:
+        model = Buyer
+        fields = ['nameBuyer', 'phoneNumberBuyer', 'emailBuyer']
+
+
+class OwnerForm(ModelForm):
+    class Meta:
+        model = Owner
+        fields = ['nameOwner', 'phoneNumberOwner', 'emailOwner']
+
+
+class FarmForm(ModelForm):
+    class Meta:
+        model = Farm
+        fields = ['nameFarm', 'ownerFarm', 'countryFarm']
+
+
+class CoffeeProductForm(ModelForm):
+    class Meta:
+        model = CoffeeProduct
+        fields = ['coffeeType',  # 'harvestYear',
+                  'farm', 'aroma', 'aftertaste', 'flavor', 'color']
+
+
+class CertificateForm(ModelForm):
+    class Meta:
+        model = Certificate
+        fields = ['body', 'product', 'dateOfExpire', 'disadvantages']
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['coffeeProduct', 'weight', 'price', 'dateOrder', 'buyer', 'purchase']
+
+
+class PaymentForm(ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['order', 'datePayment', 'amount']

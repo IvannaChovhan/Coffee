ba96a779af0b60fe41e78f9bda4c20e267a578fe
IvannaChovhan <ivanna.chovhan131@gmail.com>
2020-12-09 17:58:11 +0200

    fixed owner form

--- kursach/coffee/urls.py
+++ kursach/coffee/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('table_countries', views.table_countries_page, name='table_countries'),
     path('table_owners', views.table_owner_page, name='table_owners'),
-    path('add_row_owner', views.form_owner, name='add_row_owner'),
+    path('form_owner', views.form_owner, name='form_owner'),
 ]
--- kursach/coffee/views.py
+++ kursach/coffee/views.py
@@ -5,23 +5,6 @@ from django.urls import reverse
 from .forms import *
 
 
-def get_all_fields_from_form(instance):
-    """"
-    Return names of all available fields from given Form instance.
-
-    :arg instance: Form instance
-    :returns list of field names
-    :rtype: list
-    """
-
-    fields = list(instance().base_fields)
-
-    for field in list(instance().declared_fields):
-        if field not in fields:
-            fields.append(field)
-    return fields
-
-
 def index(request):
     return render(request, 'coffee/index.html')
 
@@ -36,7 +19,7 @@ def table_countries_page(request):
 def table_owner_page(request):
     context = {
         'title': 'Власник',
-        'link_to_add_row': reverse('coffee: form_owner')
+        'link_to_add_row': reverse('coffee:form_owner')
     }
     return render(request, 'coffee/tables_example.html', context)
 
@@ -52,11 +35,10 @@ def form_owner(request):
             error = "Сталася помилка"
 
     form = OwnerForm()
-    fields = get_all_fields_from_form(form)
+
     context = {
         'title': title,
         'form': form,
         'error': error,
-        'fields': fields
     }
     return render(request, 'coffee/form.html', context)

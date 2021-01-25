c48904d547c45210d02551ecfb239e6875a633fb
Yevhenii Hnatiuk <44451032+zhenyagnatyuk@users.noreply.github.com>
2021-01-25 19:49:49 +0200

    Update settings.py

--- kursach/kursach/settings.py
+++ kursach/kursach/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l!qff#puzpsa8zq2j3630czj%n^%d&kgy2pb57e$$&-+(5e37('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
